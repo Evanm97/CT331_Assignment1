66ef37bc69d8df1521b26635ebb75662fef2605f
Evan <evanmurray2012@gmail.com>
2018-10-25 20:20:48 +0100

    Add files via upload

--- src/q2/linkedList.c
+++ src/q2/linkedList.c
@@ -2,60 +2,98 @@
 #include <stdlib.h>
 #include <string.h>
 #include "linkedList.h"
+#define _CRT_SECURE_NO_WARNINGS
 
-typedef struct listElementStruct{
-  char* data;
-  size_t size;
-  struct listElementStruct* next;
+typedef struct listElementStruct {
+	char* data;
+	size_t size;
+	struct listElementStruct* next;
 } listElement;
 
 //Creates a new linked list element with given content of size
 //Returns a pointer to the element
-listElement* createEl(char* data, size_t size){
-  listElement* e = malloc(sizeof(listElement));
-  if(e == NULL){
-    //malloc has had an error
-    return NULL; //return NULL to indicate an error.
-  }
-  char* dataPointer = malloc(sizeof(char)*size);
-  if(dataPointer == NULL){
-    //malloc has had an error
-    free(e); //release the previously allocated memory
-    return NULL; //return NULL to indicate an error.
-  }
-  strcpy(dataPointer, data);
-  e->data = dataPointer;
-  e->size = size;
-  e->next = NULL;
-  return e;
+listElement* createEl(char* data, size_t size) {
+	listElement* e = malloc(sizeof(listElement));
+	if (e == NULL) {
+		//malloc has had an error
+		return NULL; //return NULL to indicate an error.
+	}
+	char* dataPointer = malloc(sizeof(char)*size);
+	if (dataPointer == NULL) {
+		//malloc has had an error
+		free(e); //release the previously allocated memory
+		return NULL; //return NULL to indicate an error.
+	}
+	strcpy(dataPointer, data);
+	e->data = dataPointer;
+	e->size = size;
+	e->next = NULL;
+	return e;
 }
 
 //Prints out each element in the list
-void traverse(listElement* start){
-  listElement* current = start;
-  while(current != NULL){
-    printf("%s\n", current->data);
-    current = current->next;
-  }
+void traverse(listElement* start) {
+	listElement* current = start;
+	while (current != NULL) {
+		printf("%s\n", current->data);
+		current = current->next;
+	}
 }
 
 //Inserts a new element after the given el
 //Returns the pointer to the new element
-listElement* insertAfter(listElement* el, char* data, size_t size){
-  listElement* newEl = createEl(data, size);
-  listElement* next = el->next;
-  newEl->next = next;
-  el->next = newEl;
-  return newEl;
+listElement* insertAfter(listElement* el, char* data, size_t size) {
+	listElement* newEl = createEl(data, size);
+	listElement* next = el->next;
+	newEl->next = next;
+	el->next = newEl;
+	return newEl;
 }
 
 
 //Delete the element after the given el
-void deleteAfter(listElement* after){
-  listElement* delete = after->next;
-  listElement* newNext = delete->next;
-  after->next = newNext;
-  //need to free the memory because we used malloc
-  free(delete->data);
-  free(delete);
+void deleteAfter(listElement* after) {
+	listElement* delete = after->next;
+	listElement* newNext = delete->next;
+	after->next = newNext;
+	//need to free the memory because we used malloc
+	free(delete->data);
+	free(delete);
 }
+
+int length(listElement* list) {
+	int length = 0;  // Initialize count 
+	listElement* current = list;  // Initialize current 
+	while (current != NULL)
+	{
+		length++;
+		current = current->next;
+	}
+	return length;
+	printf("Size of list %d\n", length);
+}
+
+void push(listElement** list, char* data, size_t size) {
+	listElement* newEl = createEl(data, size);
+	newEl->next = *list;
+	*list = newEl;
+}
+
+listElement* pop(listElement** list) {
+	listElement* el = *list;
+	*list = el->next;
+	free(el);
+	return el;
+}
+
+void enqueue(listElement** list, char* data, size_t size) {
+	listElement* newEl = createEl(data, size);
+	newEl->next = *list;
+	*list = newEl;
+}
+
+listElement* dequeue(listElement* list) {	listElement* current = list;
+	while (current->next->next != NULL) {
+		current = current->next;	}	list = current->next;
+	current->next = NULL;
+	return current;}
--- src/q2/tests.c
+++ src/q2/tests.c
@@ -2,24 +2,45 @@
 #include "tests.h"
 #include "linkedList.h"
 
-void runTests(){
-  printf("Tests running...\n");
-  listElement* l = createEl("Test String (1).", 30);
-  //printf("%s\n%p\n", l->data, l->next);
-  //Test create and traverse
-  traverse(l);
-  printf("\n");
+void runTests() {
+	listElement* l = createEl("String (1).", 30);
+	listElement* l2 = insertAfter(l, "String (2)", 30);
+	insertAfter(l2, "String (3)", 30);
 
-  //Test insert after
-  listElement* l2 = insertAfter(l, "another string (2)", 30);
-  insertAfter(l2, "a final string (3)", 30);
-  traverse(l);
-  printf("\n");
+	printf("\n---QUESTION 2---\n");
+	printf("---------------------------------------------------------------------\n");
+	printf("--Initial List--\n\n");
+	traverse(l);
+	printf("---------------------------------------------------------------------\n");
 
-  // Test delete after
-  deleteAfter(l);
-  traverse(l);
-  printf("\n");
+	printf("--Testing Push--\n");
+	printf("Pushing \"String (4)\" to head of list\n\n");
+	push(&l, "String (4)", 30);
+	traverse(l);
+	printf("---------------------------------------------------------------------\n");
 
-  printf("\nTests complete.\n");
+	printf("--Testing Length--\n\n");
+	traverse(l);
+	printf("\nThe length of the list is :%d", length(l));
+	printf("\n");
+	printf("---------------------------------------------------------------------\n");
+
+	printf("--Testing Pop--\n");
+	printf("Popping \"String (4)\" from head of list\n\n");
+	pop(&l);
+	traverse(l);
+	printf("---------------------------------------------------------------------\n");
+
+	printf("--Testing Dequeue--\n");
+	printf("Dequeuing \"String (3)\" from list\n\n");
+	dequeue(l);
+	traverse(l);
+	printf("---------------------------------------------------------------------\n");
+
+	printf("--Testing Enqueue--\n");
+	printf("Enqueueing \"String (5)\" to head of list\n\n");
+	push(&l, "String (5)", 30);
+	traverse(l);
+	printf("\n");
+
 }
