af09ef8fb750f25477133fe1d00e23bf8ad9d9c1
Evan <evanmurray2012@gmail.com>
2018-10-25 20:18:59 +0100

    Update assignment.c

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+void main(){
+  
+  int i;
+  int* p;
+  long l;
+  double* d;
+  char** c;
+	
+	
+  printf("\n---QUESTION 1---\n");
+  printf("---------------------------------------------------------------------\n");
+
+  printf("The size of integer is %zd\n", sizeof(i));
+  printf("The size of integer* is %zd\n", sizeof(p));
+  printf("The size of long is %zd\n", sizeof(l));
+  printf("The size of double pointer is %zd\n", sizeof(d));
+  printf("The size of char** is %zd\n", sizeof(c));
+  printf("\n\n");
+  
+  system("pause");
   return 0;
 }
+
